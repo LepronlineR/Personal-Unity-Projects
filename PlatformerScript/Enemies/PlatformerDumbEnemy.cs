a0874fbd5c53a743a7078379ab1718e530a2e2cf
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2021-01-04 18:00:23 -0500

    Removed

--- PlatformerScript/Enemies/PlatformerDumbEnemy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlatformerDumbEnemy : PlatformerEnemy{
-    //this enemy does nothing, only hurts the player if they touch it
-    
-    //smart enemy inherit from the dumb enemy basically, this is just an extra
-}
