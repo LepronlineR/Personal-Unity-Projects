55eafa58010a74fa5eb924007b233c65e2f799c2
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2021-01-05 11:20:59 -0500

    AR Things

---
+++ ARScripts/ObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] objectToSpawn;
+    private PlacementIndicator placementIndicator;
+    private int change;
+
+    public void ChangeShape(string s){
+        if(s.Equals("Cylinder")){
+            change = 0;
+        }
+        if(s.Equals("Cube")){
+            change = 1;
+        }
+        if(s.Equals("Sphere")){
+            change = 2;
+        }
+    }
+    void Start(){
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update(){
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began){
+            GameObject obj = Instantiate(objectToSpawn[change],placementIndicator.transform.position,placementIndicator.transform.rotation);
+        }
+    }
+}
